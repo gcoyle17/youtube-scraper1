934ad6ae9ab56142bb213b792f8fa5f207a4a507
gcoyle17 <59986231+gcoyle17@users.noreply.github.com>
2020-11-17 19:48:27 -0600

    Update crawler_utils.js

--- src/crawler_utils.js
+++ src/crawler_utils.js
@@ -185,10 +185,13 @@ exports.handleDetail = async (page, request) => {
         .catch((e) => handleErrorAndScreenshot(page, e, 'Getting-license-failed'));
     log.debug(`got license as ${license}`);
     
-    if (license == "You're signed out of YouTube"){
+    if (String(license) == "You're signed out of YouTube"){
         license = "True";
     }
 
+    else{
+        license = "False";
+    }
     await Apify.pushData({
         title,
         //license, // NEW --> added license variable to be pushed to Apify
